79cadc9313f07e325b3114e89328846fc0aada57
ioplker <panswitcher@yandex.ru>
2022-11-13 17:48:32 +0700

    :lipstick: Make font bigger

--- modules/general/theme.lua
+++ modules/general/theme.lua
@@ -1,4 +1,4 @@
 -- Adjust the default theme's font and size
 if not CURSES then
-  view:set_theme('base16-tomorrow-night', {font = 'Hack', size = 14})
+  view:set_theme('base16-tomorrow-night', {font = 'Hack', size = 16})
 end
